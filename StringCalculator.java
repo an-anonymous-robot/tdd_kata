135aa08d894089356d00f7eda7ed5b6967298b38
Samrat Das <dassamrat.100894@gmail.com>
2021-03-17 15:40:27 +0530

    Case 3
    The Add method can now handle new line as well as comma as delimiter

--- StringCalculator.java
+++ StringCalculator.java
@@ -5,14 +5,14 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 	public static void main(String[] args) {
-		System.out.println(Add("1,2"));
+		System.out.println(Add("1\n3,2"));
 	}
 	static int Add(String numbers) {
 		if("".equals(numbers)) {
 			return 0;
 		}
 		else {
-			List<String> arr=List.of(numbers.split(","));
+			List<String> arr=List.of(numbers.split("\\n|,"));
 			int sum=arr.stream()
 			   .map(a->Integer.parseInt(a))
 			   .reduce(0,(a,b)->a+b);
