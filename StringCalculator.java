c6eabf5b13117182332174b8a29ddeb5ec4ab4b7
Samrat Das <dassamrat.100894@gmail.com>
2021-03-17 15:59:08 +0530

    Case 4
    Add method takes custom delimiter as well ass new line and comma

--- StringCalculator.java
+++ StringCalculator.java
@@ -5,18 +5,23 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 	public static void main(String[] args) {
-		System.out.println(Add("1\n3,2"));
+		System.out.println(Add("//;\n1;3,2"));
 	}
 	static int Add(String numbers) {
 		if("".equals(numbers)) {
 			return 0;
 		}
 		else {
-			List<String> arr=List.of(numbers.split("\\n|,"));
+			String delimiter_temp=numbers.substring(2,numbers.indexOf("\n"));
+			String delimiter=delimiter_temp.concat("|\\n|,");
+			String new_str=numbers.substring(numbers.indexOf("\n")+1);
+			List<String> arr=List.of(new_str.split(delimiter));
 			int sum=arr.stream()
 			   .map(a->Integer.parseInt(a))
 			   .reduce(0,(a,b)->a+b);
 			return sum;
+//			System.out.println(delimiter);
+//			return 0;
 		}
 	}
 
